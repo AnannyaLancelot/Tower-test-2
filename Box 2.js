46f331ae97102b539bf7f2d6a52feaead8f1bb87
AnannyaLancelot <72078305+AnannyaLancelot@users.noreply.github.com>
2020-12-11 20:56:43 +0530

    Add files via upload

---
+++ Box 2.js
@@ -0,0 +1,28 @@
+class Box2{
+    constructor(x,y,width,height){
+        var options={
+            isStatic:false
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        if(this.body.speed<3){
+        var pos=this.body.position;
+        fill("pink")
+        rect(pos.x,pos.y,this.width,this.height)
+        rectMode(CENTER);
+        }else{
+            World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     rect(pos.x,pos.y,this.width,this.height)
+     pop();
+        }
+       
+        
+    }
+}
